6608d37cebb70c12f56784053d2a530d39ce6e49
Muhammad Ridwan R <jongsky_1821@yahoo.com>
2019-12-12 23:01:28 +0700

    Tugas 3.1 PHP
    Tugas Niomic

---
+++ tugas3_1.php
@@ -0,0 +1,11 @@
+<?php
+echo "Jarak kota A - Z = 360 Km <br>";
+echo "Kecepatan Motor = 90 Km/jam <br>";
+echo "Waktu Tempuh = ? <br>";
+echo "Jawaban : ";
+
+$x = 360;
+$y = 90;
+
+$z = ($x / $y) * 60;
+echo "$z Menit";
